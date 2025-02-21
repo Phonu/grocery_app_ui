3b1a097b4d07d612961d10b0b484b6dda9f45879
Phonu <50923254+Phonu@users.noreply.github.com>
2025-02-21 23:41:50 +0530

    implemented Keyboard Animation and changes in CustomerLogion page

--- src/features/auth/CustomerLogin.tsx
+++ src/features/auth/CustomerLogin.tsx
@@ -1,20 +1,50 @@
-import { View, Text, StyleSheet, SafeAreaView, Animated } from "react-native";
-import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Animated,
+  Image,
+} from "react-native";
+import React, { useEffect, useRef, useState } from "react";
 import {
   PanGestureHandler,
   GestureHandlerRootView,
   State,
 } from "react-native-gesture-handler";
+import LinearGradient from "react-native-linear-gradient";
 
 import CustomSafeAreaView from "@components/global/CustomSafeAreaView";
 import ProductSlider from "@components/login/ProductSlider";
-import { Colors } from "@utils/Constants";
+import { Colors, Fonts, lightColors } from "@utils/Constants";
 import CustomText from "@components/ui/CustomText";
 import { RFValue } from "react-native-responsive-fontsize";
 import { resetAndNavigate } from "@utils/NavigationUtils";
+import useKeyboardOffsetHeight from "@utils/useKeyboardOffsetHeight";
 
+const bottomColors = [...lightColors].reverse();
 const CustomerLogin = () => {
   const [gestureSequence, setGestureSequence] = useState<string[]>([]);
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [loading, setLoading] = useState(false);
+  const animationValue = useRef(new Animated.Value(0)).current;
+  const keyboardOffsetHeight = useKeyboardOffsetHeight();
+
+  useEffect(() => {
+    if (keyboardOffsetHeight === 0) {
+      Animated.timing(animationValue, {
+        toValue: 0,
+        duration: 500,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      Animated.timing(animationValue, {
+        toValue: -keyboardOffsetHeight * 0.84,
+        duration: 1000,
+        useNativeDriver: true,
+      }).start();
+    }
+  }, []);
 
   // TODO : Gesturehandler is not receiving the nativeEvent => NEED TO CHECK
   // sequence up up dowm left right to navigate to DeliveryScreen
@@ -40,17 +70,35 @@ const CustomerLogin = () => {
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.container}>
-        <CustomSafeAreaView>
-          <ProductSlider />
-          <PanGestureHandler onHandlerStateChange={handleGesture}>
-            <Animated.ScrollView
-              contentContainerStyle={styles.subContainer}
-              bounces={false}
-              keyboardDismissMode="on-drag"
-              keyboardShouldPersistTaps="handled"
-            ></Animated.ScrollView>
-          </PanGestureHandler>
-        </CustomSafeAreaView>
+        {/* <CustomSafeAreaView> */}
+        <ProductSlider />
+        <PanGestureHandler onHandlerStateChange={handleGesture}>
+          <Animated.ScrollView
+            contentContainerStyle={styles.subContainer}
+            bounces={false}
+            keyboardDismissMode="on-drag"
+            keyboardShouldPersistTaps="handled"
+          >
+            <LinearGradient colors={bottomColors} style={styles.gradient} />
+            <View style={styles.content}>
+              <Image
+                source={require("@assets/images/logo.jpeg")}
+                style={styles.logo}
+              />
+              <CustomText varient="h2" fontFamily={Fonts.Bold}>
+                Grocery Delivery App
+              </CustomText>
+              <CustomText
+                varient="h5"
+                fontFamily={Fonts.SemiBold}
+                style={styles.text}
+              >
+                LogIn or signUp
+              </CustomText>
+            </View>
+          </Animated.ScrollView>
+        </PanGestureHandler>
+        {/* </CustomSafeAreaView> */}
         <View style={styles.footer}>
           <SafeAreaView />
           <CustomText fontSize={RFValue(6)}>
@@ -85,6 +133,29 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
     alignItems: "center",
     marginBottom: 20,
+  },
+  gradient: {
+    paddingTop: 60,
+    width: "100%",
+  },
+  content: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    backgroundColor: "white",
+    paddingHorizontal: 20,
+    paddingBottom: 20,
+  },
+  logo: {
+    width: 50,
+    height: 50,
+    borderRadius: 20,
+    marginVertical: 10,
+  },
+  text: {
+    marginTop: 2,
+    marginBottom: 25,
+    opacity: 0.8,
   },
 });
 export default CustomerLogin;
