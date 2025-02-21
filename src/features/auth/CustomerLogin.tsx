70cf999a1d7b8cc82477176fba49cdb9134af4cc
Phonu <50923254+Phonu@users.noreply.github.com>
2025-02-21 16:27:15 +0530

    Added Gesture Handler feature.
    TODO: its not working, will take a look later.

--- src/features/auth/CustomerLogin.tsx
+++ src/features/auth/CustomerLogin.tsx
@@ -1,18 +1,55 @@
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
-import React from "react";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { View, Text, StyleSheet, SafeAreaView, Animated } from "react-native";
+import React, { useState } from "react";
+import {
+  PanGestureHandler,
+  GestureHandlerRootView,
+  State,
+} from "react-native-gesture-handler";
+
 import CustomSafeAreaView from "@components/global/CustomSafeAreaView";
 import ProductSlider from "@components/login/ProductSlider";
 import { Colors } from "@utils/Constants";
 import CustomText from "@components/ui/CustomText";
 import { RFValue } from "react-native-responsive-fontsize";
+import { resetAndNavigate } from "@utils/NavigationUtils";
 
 const CustomerLogin = () => {
+  const [gestureSequence, setGestureSequence] = useState<string[]>([]);
+
+  // TODO : Gesturehandler is not receiving the nativeEvent => NEED TO CHECK
+  // sequence up up dowm left right to navigate to DeliveryScreen
+  const handleGesture = (nativeEvent: any) => {
+    console.log("nativeEvent", nativeEvent);
+    if (nativeEvent.state === State.END) {
+      const { translationX, translationY } = nativeEvent;
+      let direction = "";
+
+      if (Math.abs(translationX) > Math.abs(translationY)) {
+        direction = translationX > 0 ? "right" : "left";
+      } else {
+        direction = translationY > 0 ? "down" : "up";
+      }
+
+      const newSequence = [...gestureSequence, direction].slice(-5);
+      setGestureSequence(newSequence);
+      if (newSequence?.join(" ") === "up up down left right") {
+        resetAndNavigate("DeliveryLogin");
+      }
+    }
+  };
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.container}>
         <CustomSafeAreaView>
           <ProductSlider />
+          <PanGestureHandler onHandlerStateChange={handleGesture}>
+            <Animated.ScrollView
+              contentContainerStyle={styles.subContainer}
+              bounces={false}
+              keyboardDismissMode="on-drag"
+              keyboardShouldPersistTaps="handled"
+            ></Animated.ScrollView>
+          </PanGestureHandler>
         </CustomSafeAreaView>
         <View style={styles.footer}>
           <SafeAreaView />
@@ -42,6 +79,12 @@ const styles = StyleSheet.create({
     padding: 10,
     backgroundColor: "#f8f9fc",
     width: "100%",
+  },
+  subContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    marginBottom: 20,
   },
 });
 export default CustomerLogin;
